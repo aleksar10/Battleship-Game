35a37418ac767f135b1c7229c996949ad513a70e
Aleksandra Rozmus <146929867+aleksar10@users.noreply.github.com>
2026-03-30 13:47:33 +0200

    Update Network.java

--- network/Network.java
+++ network/Network.java
@@ -17,7 +17,7 @@ public class Network {
     public Network(Socket socket, GameLogger logger) throws Exception {
         this.socket = socket;
         this.logger = logger;
-        socket.setSoTimeout(TIMEOUT);
+        //socket.setSoTimeout(TIMEOUT);
         in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
         out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
     }
